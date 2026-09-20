c6e5cc3ba71a1a3d81ffa55368f2e22968922e38
Derek <derex1987@gmail.com>
2026-09-20 13:12:50 -0500

    feat: site data

---
+++ src/data/site.ts
@@ -0,0 +1,15 @@
+export const site = {
+  name: "derex smp",
+  homeUrl: "https://derex-smp.github.io/",
+  worldMapUrl: "https://mc.longhorns.dev/map/",
+  youtubeUrl: "https://www.youtube.com/hashtag/derexsmp",
+};
+
+export const navigationLinks = [
+  { label: 'Home', href: site.homeUrl },
+  { label: 'World Map', href: site.worldMapUrl },
+  { label: 'Members', href: '/members' },
+  { label: 'Wall of Shame', href: '/notbanlist' },
+  { label: 'Youtube', href: site.youtubeUrl },
+  { label: 'Legacy Rules', href: '/serverrules.html' },
+] as const;
